3f3aaeed59502494c8952f48edd4969a1c1ba461
Rancho Cordova <Rancho Cordova>
2017-02-24 11:25:50 -0800

    Fixed login script Thomas and added additional scripts to test search

--- spec/homepage/autocompleter_spec.rb
+++ spec/homepage/autocompleter_spec.rb
@@ -1,107 +1,138 @@
 describe 'Homepage Autocompleter', type: :feature do
+# Perform log in prior to all tests
+  before do
+      visit '/'
+      username_input = page.find('input[name=username]')
+      username_input.send_keys 'guest'
+      password_input = page.find('input[name=password]')
+      password_input.send_keys 'guest'
+      click_button('Login')
+  end
+
+  # Checks to ensure login is successful
   it 'log on to Login page' do
     visit '/'
-    username_input = page.find('input[name=username]')
-    username_input.send_keys 'guest'
-    password_input = page.find('input[name=password]')
-    password_input.send_keys 'guest'
-    click_button('Login')
-    # search bar appears by checking for the label "People"
     expect(page).to have_content 'People'
   end
 
-  it 'finds a result by first name' do
+  # validate specific person is in suggestion list when searched by last name
+  it 'find a result by a specific first name' do
     visit '/'
-    username_input = page.find('input[name=username]')
-    username_input.send_keys 'guest'
-    password_input = page.find('input[name=password]')
-    password_input.send_keys 'guest'
-    click_button('Login')
     autocompleter_fill_in 'People', 'Houston'
     within '.react-autosuggest__suggestions-list' do
-          expect(page).to have_content 'Houston de Griff'
+      expect(page).to have_content 'Houston de Griff'
     end
   end
 
-  it 'finds a result by last name' do
+  # validate specific person is in suggestion list when searched by last name
+  it 'find a result by a specific last name' do
     visit '/'
-    username_input = page.find('input[name=username]')
-    username_input.send_keys 'guest'
-    password_input = page.find('input[name=password]')
-    password_input.send_keys 'guest'
-    click_button('Login')
     autocompleter_fill_in 'People', 'Griff'
     within '.react-autosuggest__suggestions-list' do
       expect(page).to have_content 'Houston de Griff'
     end
   end
 
-  # validating that searching on "Pete" would return "Peter" records as well.
+  # validate specific person is in suggestion list when searched by DOB
   it 'finds a result by DOB' do
     visit '/'
-    username_input = page.find('input[name=username]')
-    username_input.send_keys 'guest'
-    password_input = page.find('input[name=password]')
-    password_input.send_keys 'guest'
-    click_button('Login')
     autocompleter_fill_in 'People', '1905'
     within '.react-autosuggest__suggestions-list' do
       expect(page).to have_content 'Boris Badboy'
     end
   end
 
-  # validating that searching on "Pete" would return "Peter" records as well.
-  it 'Partial Name Search on Pete - Part I' do
+
+  # validate that searching on "Pete" would return "Pete" records
+  it 'Validating all records contain partial search criteria - Pt 1' do
     visit '/'
-    username_input = page.find('input[name=username]')
-    username_input.send_keys 'guest'
-    password_input = page.find('input[name=password]')
-    password_input.send_keys 'guest'
-    click_button('Login')
     autocompleter_fill_in 'People', 'Pete'
-    within '.react-autosuggest__suggestions-list' do
-      expect(page).to have_content 'Pete'
-    end
-  end
-
-  it 'Partial Name Search on Pete - Part II' do
-    visit '/'
-    username_input = page.find('input[name=username]')
-    username_input.send_keys 'guest'
-    password_input = page.find('input[name=password]')
-    password_input.send_keys 'guest'
-    click_button('Login')
-    autocompleter_fill_in 'People', 'Pete'
-    within '.react-autosuggest__suggestions-list' do
-      expect(page).to have_content 'Peter'
-    end
-  end
-
-  # validating that searching on partial year would return record with 199X year
-  it 'Partial Search on DOB' do
-    visit '/'
-    username_input = page.find('input[name=username]')
-    username_input.send_keys 'guest'
-    password_input = page.find('input[name=password]')
-    password_input.send_keys 'guest'
-    click_button('Login')
-    autocompleter_fill_in 'People', '199'
-    within '.react-autosuggest__suggestions-list' do
-      expect(page).to have_content '199'
-    end
-  end
-
-  # validating that searching on single char Mon Day would return record with '3/3/'
-  it 'checking partial date M/D/' do
-    visit '/'
-      username_input = page.find('input[name=username]')
-      username_input.send_keys 'guest'
-      password_input = page.find('input[name=password]')
-      password_input.send_keys 'guest'
-      click_button('Login')
-      autocompleter_fill_in 'People', '3/3/'
-      within '.react-autosuggest__suggestions-list' do
-        expect(page).to have_content '3/3/'
+    within('ul.react-autosuggest__suggestions-list') do
+      page.all('li').each do |element|
+        expect(element.text).to have_content('Pete')
       end
     end
   end
+
+  # validate that searching on "Pete" would return "Peter" records as well.
+  it 'Validating all records contain partial search criteria - Pt 2' do
+    visit '/'
+    autocompleter_fill_in 'People', 'Pete'
+    within('ul.react-autosuggest__suggestions-list') do
+      page.all('li').each do |element|
+        expect(element.text).to have_content('Peter')
+      end
+    end
+  end
+
+  # Validating all records for 9-digit SSN is being returned
+  it 'Validating all SSN records returned' do
+    visit '/'
+    autocompleter_fill_in 'People', '123456789'
+    within('ul.react-autosuggest__suggestions-list') do
+      page.all('li').each do |element|
+        expect(element.text).to have_content('6789')
+      end
+    end
+  end
+
+  # validate every record on the suggestion list contains the search
+  # criteria whole year'
+  it 'Validating only DOB search data is returned' do
+    visit '/'
+    autocompleter_fill_in 'People', '1999'
+    within('ul.react-autosuggest__suggestions-list') do
+      page.all('li').each do |element|
+        expect(element.text).to have_content('1999')
+      end
+    end
+  end
+
+  # validate every record on the suggestion list contains the search
+  # criteria partial year'
+  it 'Validating only DOB search data is returned' do
+    visit '/'
+    autocompleter_fill_in 'People', '199'
+    within('ul.react-autosuggest__suggestions-list') do
+      page.all('li').each do |element|
+        expect(element.text).to have_content('199')
+      end
+    end
+  end
+
+  # validate every record on the suggestion list contains the partial search
+  # criteria using no leading zeroes DOB'
+  it 'Validating only DOB search data is returned - Pt 1' do
+    visit '/'
+    autocompleter_fill_in 'People', '3/3/'
+    within('ul.react-autosuggest__suggestions-list') do
+      page.all('li').each do |element|
+        expect(element.text).to have_content('3/3/')
+      end
+    end
+  end
+
+  # validate every record on the suggestion list contains the partial search
+  # criteria using leading zeroes DOB'
+  it 'Validating only DOB search data is returned - Pt 2' do
+    visit '/'
+    autocompleter_fill_in 'People', '03/03/'
+    within('ul.react-autosuggest__suggestions-list') do
+      page.all('li').each do |element|
+        expect(element.text).to have_content('3/3/')
+      end
+    end
+  end
+
+  # validate every record on the suggestion list contains the search criteria
+  # using leading zeroes entire DOB'
+  it 'Validating only DOB search data is returned - Pt 3' do
+    visit '/'
+    autocompleter_fill_in 'People', '03/03/1990'
+    within('ul.react-autosuggest__suggestions-list') do
+      page.all('li').each do |element|
+        expect(element.text).to have_content('3/3/1990')
+      end
+    end
+  end
+end
