cf7a5a46f8ac946b150d9c5b526617cf5be8fd15
Rancho Cordova <Rancho Cordova>
2017-03-30 09:11:32 -0700

    qa test for the narrative card

---
+++ spec/screening/narrative_card_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+describe 'Narrative card', type: :feature do
+  # Selecting Create Person on homepage
+  it 'Testing narrative card' do
+    visit '/'
+    click_link 'Start Screening'
+    within '#narrative-card' do
+      within '.card-header' do
+        expect(page).to have_content('NARRATIVE')
+      end
+      # Saving a blank card and validating
+      click_button 'Save'
+      within '.card-body' do
+        expect(page).to have_content('Report Narrative')
+      end
+      within '.card-header' do
+        expect(page).to have_content('NARRATIVE')
+        find(:css, 'i.fa.fa-pencil').click
+      end
+      within '.card-body' do
+        fill_in 'Report Narrative',
+                with: 'Fr1ends, Rom@ns, countrymen, 1end me your ears;'
+        click_button 'Save'
+        expect(page).to have_content(
+          'Fr1ends, Rom@ns, countrymen, 1end me your ears;'
+        )
+      end
+      within '.card-header' do
+        find(:css, 'i.fa.fa-pencil').click
+      end
+      within '.card-body' do
+        expect(page).to have_field(
+          'Report Narrative',
+          with: 'Fr1ends, Rom@ns, countrymen, 1end me your ears;'
+        )
+        fill_in 'Report Narrative', with: 'Et Tu Brute?'
+        # Verify new data was not saved on 'Cancel'
+        click_button 'Cancel'
+        expect(page).to have_content(
+          'Fr1ends, Rom@ns, countrymen, 1end me your ears;'
+        )
+      end
+    end
+  end
+end
