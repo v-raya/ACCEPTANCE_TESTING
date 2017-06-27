04a12e52f82493011a26ab48335ff7d0b39283ba
John Gedeon <John.Gedeon@OSI.ca.gov>
2017-06-26 17:11:30 -0700

    Add new helpers for testing validations
    - Update fill_in_datepicker code to latest from intake
    - Add focus and blur helpers to facilitate checking validation messages
    
    [#146334465]

--- spec/shame_helpers.rb
+++ spec/shame_helpers.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
-def fill_in_datepicker(locator, with:)
+DATE_FORMATS = {
+  'Date' => '%m/%d/%Y',
+  'Time' => '%m/%d/%y %l:%m %p',
+  'DateTime' => '%m/%d/%y%l:%m %p',
+  'ActiveSupport::TimeWithZone' => '%m/%d/%y %l:%m %p'
+}.freeze
+
+def fill_in_datepicker(locator, with:, blur: true)
   date_string = date_to_string(with)
   # change events for the datepicker are only firing correctly after the
   # second event because of differences in how capybara is triggering
@@ -8,15 +15,28 @@ def fill_in_datepicker(locator, with:)
   # need to trigger that by clicking on an arbitrary element.
   fill_in(locator, with: date_string)
   fill_in(locator, with: date_string)
-  first('*').click
+  first('*').click if blur
 end
 
 def date_to_string(date_object)
-  if date_object.class == Date
-    date_object.strftime('%m/%d/%Y')
-  elsif date_object.class == Time
-    date_object.strftime('%m/%d/%Y %l:%M %p')
+  date_format = DATE_FORMATS[date_object.class.to_s]
+
+  if date_format
+    date_object.strftime(date_format)
   else
     date_object
   end
+end
+
+def focus(locator)
+  element = page.find_field(locator)
+  if (element.tag_name == 'select')
+    execute_script("$('##{element['id']}').trigger('focus')")
+  else
+    element.click
+  end
+end
+
+def blur
+  first('*').native.click
 end
