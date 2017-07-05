e4166e314fece29805009b232b4a34f2745a7aaa
Thomas Ramirez <Thomas.Ramirez@OSI.ca.gov>
2017-07-05 14:43:26 -0700

    Add legacy ID to case and referral matchers
    [#147278147]

--- spec/support/matchers/have_case.rb
+++ spec/support/matchers/have_case.rb
@@ -4,6 +4,7 @@ RSpec::Matchers.define :have_case do |expected|
   match do |actual|
     start_date = expected[:start_date]
     end_date = expected[:end_date]
+    case_id = expected[:case_id]
     status = expected[:status]
     service_component = expected[:service_component]
     county = expected[:county]
@@ -12,7 +13,7 @@ RSpec::Matchers.define :have_case do |expected|
     parents = expected[:parents]
 
     expect(actual).to have_content [start_date, end_date].compact.join(' - ')
-    expect(actual).to have_content "Case (#{[status, service_component].join(' - ')})"
+    expect(actual).to have_content "Case #{case_id} (#{[status, service_component].join(' - ')})"
     expect(actual).to have_content county if county
     expect(actual).to have_content "Focus Child: #{focus_child}"
     expect(actual).to have_content "Parent(s): #{parents.join(', ')}"
