c4c7b11f898e32605a4a3e97deeb4b799239ea85
Sara Bocciardi <Sara.Bocciardi@OSI.ca.gov>
2017-07-25 10:29:12 -0700

    Make sure script will run in both preint and int

--- spec/screening/relationship_card_spec.rb
+++ spec/screening/relationship_card_spec.rb
@@ -2,16 +2,17 @@
 
 person1 = {
   fname: 'Marty',
-  lname: 'R.'
+  lname: 'R'
 }
 person2 = {
   fname: 'Ricky',
-  lname: 'W.'
+  lname: 'W'
 }
 
 describe 'Relationship card QA test', type: :feature do
   # Selecting Start Screening from homepage
   # Note - This script is dependent on established seed data in the environment
+  person1_full_name, person2_full_name = nil
 
   before do
     visit '/'
@@ -27,39 +28,46 @@ describe 'Relationship card QA test', type: :feature do
     end
     # Adding an existing child to a screening
     within '#search-card', text: 'Search' do
-      autocompleter_fill_in 'Search for any person', person1[:fname]
-      find('li', text: "#{person1[:fname]} #{person1[:lname]}").click
+      search_string = "#{person1[:fname]} #{person1[:lname]}"
+      autocompleter_fill_in 'Search for any person', search_string
+      matching_result = first('li', text: search_string)
+      person1_full_name = matching_result.first('strong').text
+      matching_result.click
     end
 
     within '#relationships-card' do
       # Verify rendering of card
-      expect(page).to have_content("#{person1[:fname]} #{person1[:lname]} is the...")
-      expect(page).to have_content('Son  of Missy R.')
-      expect(page).to have_content('Son of Ricky W.')
-      expect(page).to have_content('Brother of Roland W.')
-      expect(page).to have_content('Brother  of Sharon W.')
+      expect(page).to have_content("#{person1_full_name} is the...")
+      expect(page).to have_content('Son  of Missy R')
+      expect(page).to have_content('Son of Ricky W')
+      expect(page).to have_content('Brother of Roland W')
+      expect(page).to have_content('Brother  of Sharon W')
     end
 
     # Adding an existing adult that is related to an existing child on a screening
     within '#search-card', text: 'Search' do
-      autocompleter_fill_in 'Search for any person', person2[:fname]
-      find('li', text: "#{person2[:fname]} #{person2[:lname]}").click
+      search_string = "#{person2[:fname]} #{person2[:lname]}"
+      autocompleter_fill_in 'Search for any person', search_string
+      matching_result = first('li', text: search_string)
+      person2_full_name = matching_result.first('strong').text
+      matching_result.click
     end
+
     person2_id = find('div[id^="participants-card-"]', text: person2[:fname])[:id]
     person2_card = find('#' + person2_id)
 
     within '#relationships-card' do
       # Verify rendering of card
-      expect(page).to have_content("#{person1[:fname]} #{person1[:lname]} is the...")
-      expect(page).to have_content('Son  of Missy R.')
-      expect(page).to have_content('Son of Ricky W.')
-      expect(page).to have_content('Brother of Roland W.')
-      expect(page).to have_content('Brother  of Sharon W.')
-      expect(page).to have_content("#{person2[:fname]} #{person2[:lname]} is the...")
-      expect(page).to have_content('Father  of Marty R.')
-      expect(page).to have_content('Spouse of Missy R.')
-      expect(page).to have_content('Father of Roland W.')
-      expect(page).to have_content('Father  of Sharon W.')
+      expect(page).to have_content("#{person1_full_name} is the...")
+      expect(page).to have_content('Son  of Missy R')
+      expect(page).to have_content('Son of Ricky W')
+      expect(page).to have_content('Brother of Roland W')
+      expect(page).to have_content('Brother  of Sharon W')
+      expect(page).to have_content("#{person2_full_name} is the...")
+      expect(page).to have_content('Father  of Marty R')
+      expect(page).to have_content('Spouse of Missy R')
+      expect(page).to have_content('Father of Roland W')
+      expect(page).to have_content('Father  of Sharon W')
     end
 
     # Deleting a person that was added to a screening.
@@ -68,16 +76,16 @@ describe 'Relationship card QA test', type: :feature do
     end
     within '#relationships-card' do
       # Verify rendering of card
-      expect(page).to have_content("#{person1[:fname]} #{person1[:lname]} is the...")
-      expect(page).to have_content('Son  of Missy R.')
-      expect(page).to have_content('Son of Ricky W.')
-      expect(page).to have_content('Brother of Roland W.')
-      expect(page).to have_content('Brother  of Sharon W.')
-      expect(page).not_to have_content("#{person2[:fname]} #{person2[:lname]} is the...")
-      expect(page).not_to have_content('Father  of Marty R.')
-      expect(page).not_to have_content('Spouse of Missy R.')
-      expect(page).not_to have_content('Father of Roland W.')
-      expect(page).not_to have_content('Father  of Sharon W.')
+      expect(page).to have_content("#{person1_full_name} is the...")
+      expect(page).to have_content('Son  of Missy R')
+      expect(page).to have_content('Son of Ricky W')
+      expect(page).to have_content('Brother of Roland W')
+      expect(page).to have_content('Brother  of Sharon W')
+      expect(page).not_to have_content("#{person2_full_name} is the...")
+      expect(page).not_to have_content('Father  of Marty R')
+      expect(page).not_to have_content('Spouse of Missy R')
+      expect(page).not_to have_content('Father of Roland W')
+      expect(page).not_to have_content('Father  of Sharon W')
     end
   end
 end
