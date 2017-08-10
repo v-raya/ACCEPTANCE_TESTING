3081138855153fd8d4d6e6146e9563698b578248
Aman Alshurafa <aman.alshurafa@osi.ca.gov>
2017-08-09 19:14:57 -0700

    Fix Rubocop offense method has too many lines and line too long

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,9 +41,7 @@ module AcceptanceTesting
       config.default_max_wait_time  = 10
     end
 
-    Capybara.register_driver :chrome do |app|
-      Capybara::Selenium::Driver.new(app, browser: :chrome)
-    end
+    Capybara.register_driver :chrome { |a| Capybara::Selenium::Driver.new(a, browser: :chrome) }
     Capybara.enable_aria_label = true
 
     Capybara::Webkit.configure(&:allow_unknown_urls)
