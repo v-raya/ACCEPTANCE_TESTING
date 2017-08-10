dd5dd13f66576e32d0d988fb45455fa7a5abd9a3
Aman Alshurafa <aman.alshurafa@osi.ca.gov>
2017-08-09 18:47:08 -0700

    Clean up & improve participant helpers:
    - Allow overriding addresses for reporter
    - Allow randomizing roles
    - Correct rubocop offenses
    - Random participant with randomized roles

--- spec/participant_helpers.rb
+++ spec/participant_helpers.rb
@@ -3,6 +3,11 @@
 require 'address_helpers'
 
 module Participant
+  def self.keys
+    %i[roles first_name middle_name last_name name_suffix gender ssn date_of_birth
+       races ethnicity languages addresses]
+  end
+
   def self.all_ethnicity_details
     ['Mexican', 'Costa Rican', 'Mayan']
   end
@@ -12,13 +17,11 @@ module Participant
   end
 
   def self.all_languages
-    [
-      'American Sign Language', 'Arabic', 'Armenian', 'Cambodian', 'Cantonese', 'English', 'Farsi',
-      'French', 'German', 'Hawaiian', 'Hebrew', 'Hmong', 'Ilocano', 'Indochinese', 'Italian',
-      'Japanese', 'Korean', 'Lao', 'Mandarin', 'Mien', 'Other Chinese', 'Other Non-English',
-      'Polish', 'Portuguese', 'Romanian', 'Russian', 'Samoan', 'Sign Language (Not ASL)',
-      'Spanish', 'Tagalog', 'Thai', 'Turkish', 'Vietnamese'
-    ]
+    ['American Sign Language', 'Arabic', 'Armenian', 'Cambodian', 'Cantonese', 'English', 'Farsi',
+     'French', 'German', 'Hawaiian', 'Hebrew', 'Hmong', 'Ilocano', 'Indochinese', 'Italian',
+     'Japanese', 'Korean', 'Lao', 'Mandarin', 'Mien', 'Other Chinese', 'Other Non-English',
+     'Polish', 'Portuguese', 'Romanian', 'Russian', 'Samoan', 'Sign Language (Not ASL)',
+     'Spanish', 'Tagalog', 'Thai', 'Turkish', 'Vietnamese']
   end
 
   def self.all_races
@@ -26,112 +29,105 @@ module Participant
   end
 
   def self.all_roles
-    [
-      'Victim', 'Perpetrator', 'Mandated Reporter', 'Non-mandated Reporter', 'Anonymous Reporter'
-    ]
+    ['Victim', 'Perpetrator', 'Mandated Reporter', 'Non-mandated Reporter', 'Anonymous Reporter']
   end
 
   def self.all_race_details
     %w[European Caribbean Korean Ethiopian]
   end
 
-  def self.first_name
-    FFaker::Name.first_name
+  def self.roles(value = nil)
+    value || all_roles.sample(2)
   end
 
-  def self.middle_name
-    FFaker::Product.letters(1)
+  def self.first_name(value = nil)
+    value || FFaker::Name.first_name
   end
 
-  def self.last_name
-    FFaker::Name.last_name
+  def self.middle_name(value = nil)
+    value || FFaker::Product.letters(1)
   end
 
-  def self.name_suffix
-    FFaker::Name.suffix
+  def self.last_name(value = nil)
+    value || FFaker::Name.last_name
   end
 
-  def self.gender
-    Participant.all_genders.sample
+  def self.name_suffix(value = nil)
+    value || FFaker::Name.suffix
   end
 
-  def self.ssn
-    FFaker::SSN.ssn
+  def self.gender(value = nil)
+    value || Participant.all_genders.sample
   end
 
-  def self.date_of_birth
-    FFaker::Time.between(Time.new(2000), Time.new(2017)).strftime('%m/%d/%Y')
+  def self.ssn(value = nil)
+    value || FFaker::SSN.ssn
   end
 
-  def self.race
-    Participant.all_races.sample
+  def self.date_of_birth(value = nil)
+    value || FFaker::Time.between(Time.new(2000), Time.new(2017)).strftime('%m/%d/%Y')
   end
 
-  def self.race_detail
-    Participant.all_race_details.sample
+  def self.race(value = nil)
+    value || all_races.sample
   end
 
-  def self.races
-    Array.new(rand(0..2)) { { race: race, race_detail: race_detail } }
+  def self.race_detail(value = nil)
+    value || all_race_details.sample
   end
 
-  def self.hispanic_latino_origin
-    ['Yes', 'No', nil, nil].sample
+  def self.races(value = nil)
+    value || Array.new(rand(0..2)) { { race: race, race_detail: race_detail } }
   end
 
-  def self.ethnicity_detail
-    Participant.all_ethnicity_details.sample
+  def self.hispanic_latino_origin(value = nil)
+    value || ['Yes', 'No', nil, nil].sample
   end
 
-  def self.ethnicity
-    hispanic_latino = hispanic_latino_origin
-    {
-      hispanic_latino_origin: hispanic_latino,
-      ethnicity_detail: hispanic_latino ? ethnicity_detail : nil
-    }
+  def self.ethnicity_detail(value = nil)
+    value || all_ethnicity_details.sample
   end
 
-  def self.languages
-    Participant.all_languages.sample(rand(3))
+  def self.ethnicity(value = nil)
+    value || hispanic_latino = hispanic_latino_origin
+    { hispanic_latino_origin: hispanic_latino,
+      ethnicity_detail: hispanic_latino ? ethnicity_detail : nil }
   end
 
-  def self.addresses
-    Array.new(rand(0..2)) { Address.full_address }
+  def self.languages(value = nil)
+    value || all_languages.sample(rand(3))
+  end
+
+  def self.addresses(value = nil)
+    value || Array.new(rand(0..2)) { Address.full_address }
+  end
+
+  def self.random(attrs = {})
+    participant = {}
+    Participant.keys.each do |key|
+      participant[key] = Participant.send(key, attrs[key])
+    end
+    participant
   end
 
   def self.collateral(attrs = {})
-    {
-      roles: [],
-      first_name: attrs[:first_name] || Participant.first_name,
-      middle_name: attrs[:middle_name] || Participant.middle_name,
-      last_name: attrs[:last_name] || Participant.last_name,
-      name_suffix: attrs[:name_suffix] || Participant.name_suffix,
-      gender: attrs[:gender] || Participant.gender,
-      ssn: attrs[:ssn] || Participant.ssn,
-      date_of_birth: attrs[:date_of_birth] || Participant.date_of_birth,
-      races: attrs[:races] || Participant.races,
-      ethnicity: attrs[:ethnicity] || Participant.ethnicity,
-      languages: attrs[:languages] || Participant.languages,
-      addresses: attrs[:addresses] || []
-    }
+    attrs[:roles] = []
+    random(attrs)
   end
 
   def self.victim(attrs = {})
-    participant = collateral(attrs)
-    participant[:roles] = ['Victim']
-    participant
+    attrs[:roles] = ['Victim']
+    random(attrs)
   end
 
   def self.perpetrator(attrs = {})
-    participant = collateral(attrs)
-    participant[:roles] = ['Perpetrator']
-    participant
+    attrs[:roles] = ['Perpetrator']
+    random(attrs)
   end
 
   def self.reporter(attrs = {})
-    participant = collateral(attrs)
-    participant[:roles] = ['Mandated Reporter']
-    participant[:addresses] = [Address.full_address]
-    participant
+    attrs[:roles] = ['Mandated Reporter']
+    attrs[:addresses] ||= [Address.full_address]
+    random(attrs)
   end
 end
