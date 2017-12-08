f3e985f3d6dc27af778cd351694b7e873d44c6e2
Thomas Ramirez <thomas.ramirez@osi.ca.gov>
2017-12-07 16:27:32 -0800

    Clean up old tests
    - use current data
    - wait for table to appear
    
    [#31]

--- spec/screening/hoi_card_with_cases_and_referrals_spec.rb
+++ spec/screening/hoi_card_with_cases_and_referrals_spec.rb
@@ -85,126 +85,78 @@ describe 'History Card with cases and referrals', type: :feature do
       within '#history-card' do
         referral_rows = page.all('tr', text: 'Referral')
         expect(referral_rows.count).to eq 1
-
-        case_rows = page.all('tr', text: 'Case')
-        expect(case_rows.count).to eq 0
-      end
-    end
-
-    within '.container' do
-      screening_page.add_person_from_search(additional_info: shared_hoi_3[:dob], name: shared_hoi_3[:name])
-      expect(page).to have_content shared_hoi_3[:name]
-
-      within '#history-card' do
-        referral_rows = page.all('tr', text: 'Referral')
-        expect(referral_rows.count).to eq 1
-
-        case_rows = page.all('tr', text: 'Case')
-        expect(case_rows.count).to eq 0
       end
     end
   end
 
-  let(:open_referral) { { dob: '1955-03-31', first_name: 'Alex', legacy_id: '5YA8w0Q04Z' } }
+  let(:complete_referral) { { dob: '08/12/1994', name: 'Robert C Chaman', legacy_id: '0199-8095-0742-9001316' } }
 
-  it 'displays an open referral correctly' do
-    skip 'This test is intermittent'
-    screening_page.add_person_from_search(additional_info: open_referral[:dob], name: open_referral[:first_name])
+  it 'displays a complete referral correctly' do
+    screening_page.add_person_from_search(additional_info: complete_referral[:dob], name: complete_referral[:name])
 
     within '#history-card' do
-      referral_rows = page.all('tr', text: 'Referral')
-      expect(referral_rows.count).to eq 1
+      within 'table.table-hover' do
+        referral_rows = page.all('tr', text: 'Referral')
+        expect(referral_rows.count).to eq 3
 
-      within referral_rows[0] do
-        expect(page).to have_referral(start_date: '05/08/1996',
-                                      status: 'Open',
-                                      referral_id: '0489-9008-5232-2000283',
-                                      county: 'Modoc',
-                                      response_time: 'Immediate',
-                                      worker: 'Tester W',
-                                      allegations: [{
-                                        victim: 'Alexandra Z',
-                                        perpetrator: 'Alexandra Z',
-                                        type: 'Emotional Abuse',
-                                        disposition: 'Pending decision'
-                                      }, {
-                                        victim: 'Alejandro Z',
-                                        perpetrator: 'Alexandra Z',
-                                        type: 'Severe Neglect',
-                                        disposition: 'Pending decision'
-                                      }, {
-                                        victim: 'Alejandro Z',
-                                        perpetrator: 'Alejandro Z',
-                                        type: 'Physical Abuse',
-                                        disposition: 'Pending decision'
-                                      }])
+        within referral_rows[2] do
+          expect(page).to have_referral(
+            start_date: '01/18/2001',
+            end_date: '01/19/2001',
+            status: 'Closed',
+            referral_id: '1251-0655-3661-3001316',
+            county: 'Imperial',
+            response_time: '5 Day',
+            worker: 'North ER Worker 2 ER2',
+            allegations: [
+              {
+                victim: 'Robert Chaman',
+                perpetrator: 'Emily Reinger',
+                type: 'Physical Abuse',
+                disposition: 'Substantiated'
+              }, {
+                victim: 'Alyssa Twamley',
+                perpetrator: 'Emily Reinger',
+                type: 'At Risk, sibling abused',
+                disposition: 'Substantiated'
+              }, {
+                victim: 'Ty Gossart',
+                perpetrator: 'Emily Reinger',
+                type: 'At Risk, sibling abused',
+                disposition: 'Substantiated'
+              }
+            ]
+          )
+        end
       end
     end
   end
 
-  let(:closed_case) { { first_name: 'Bobby', last_name: 'W', legacy_id: 'ETSbL6a0Dv' } }
+  let(:complete_case) { { first_name: 'Robert', last_name: 'Chaman', legacy_id: '0199-8095-0742-9001316' } }
 
-  it 'displays a closed case correctly' do
-    full_name = "#{closed_case[:first_name]} #{closed_case[:last_name]}"
+  it 'displays a complete case correctly' do
+    full_name = "#{complete_case[:first_name]} C #{complete_case[:last_name]}"
+    focus_name = "#{complete_case[:first_name]} #{complete_case[:last_name]}"
     screening_page.add_person_from_search(name: full_name)
 
     within '#history-card' do
-      case_rows = page.all('tr', text: 'Case')
-      expect(case_rows.count).to eq 1
+      within 'table.table-hover' do
+        case_rows = page.all('tr', text: 'Case')
+        expect(case_rows.count).to eq 2
 
-      within case_rows[0] do
-        expect(page).to have_case(start_date: '01/25/2000',
-                                  end_date: '09/26/2002',
-                                  case_id: '0238-4474-6454-8000863',
-                                  status: 'Closed',
-                                  county: 'Monterey',
-                                  service_component: 'Family Maintenance',
-                                  focus_child: full_name,
-                                  parents: ['Dolly W'],
-                                  worker: 'Mary J')
-      end
-    end
-  end
-
-  let(:referral_with_reporter) { { name: 'Jimmy L', legacy_id: 'M0Vajck0NC' } }
-
-  it 'displays the reporter for referrals' do
-    screening_page.add_person_from_search(name: referral_with_reporter[:name])
-
-    within '#history-card' do
-      referral_rows = page.all('tr', text: 'Referral')
-      expect(referral_rows.count).to eq 1
-
-      within referral_rows[0] do
-        expect(page).to have_referral(
-          start_date: '08/16/2002',
-          referral_id: '0832-3822-4061-4001438',
-          status: 'Open',
-          county: 'San Diego',
-          response_time: '10 Day',
-          worker: 'Alphanette D',
-          reporter: 'Mary L',
-          allegations: [{
-            victim: 'Motry B',
-            type: 'Caretaker Absence/Incapacity ',
-            disposition: 'Pending decision'
-          }, {
-            victim: 'Tommy M',
-            perpetrator: 'Motry B',
-            type: 'Sexual Abuse',
-            disposition: 'Substantiated'
-          }, {
-            victim: 'Jimmy L',
-            perpetrator: 'Tommy M',
-            type: 'Sexual Abuse',
-            disposition: 'Substantiated'
-          }, {
-            victim: 'Jimmy L',
-            perpetrator: 'Motry B',
-            type: 'Sexual Abuse',
-            disposition: 'Substantiated'
-          }]
-        )
+        within case_rows[1] do
+          expect(page).to have_case(
+            start_date: '01/18/2001',
+            end_date: '01/19/2001',
+            case_id: '1194-4391-5581-3001320',
+            status: 'Closed',
+            county: 'Imperial',
+            service_component: 'Family Maintenance',
+            focus_child: focus_name,
+            parents: ['Robert Gilpillan', 'Emily Reinger'],
+            worker: 'North ER Worker 2 ER2'
+          )
+        end
       end
     end
   end
