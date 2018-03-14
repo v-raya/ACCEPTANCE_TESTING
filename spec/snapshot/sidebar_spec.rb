2d7a9008063ab5a1a3c66f23954adefffa8a3d54
manish pokharel <manish.pokharel@osi.ca.gov>
2018-03-13 21:47:34 -0700

    Added test for the list of anchor links

--- spec/snapshot/sidebar_spec.rb
+++ spec/snapshot/sidebar_spec.rb
@@ -8,8 +8,15 @@ describe 'Snapshot Index', type: :feature do
   end
 
   it 'has a sidebar' do
-    expect(page).to have_link('People & Roles', :href => '#search-card-anchor')
-    expect(page).to have_link('Relationships', :href => '#relationships-card-anchor')
-    expect(page).to have_link('History', :href => '#history-card-anchor')
+    expect(page).to have_css('.side-bar')
   end
+
+  it 'has list of anchor links on the left hand side of the page' do
+    within '.side-bar' do
+      expect(page).to have_link('People & Roles', href: '#search-card-anchor')
+      expect(page).to have_link('Relationships', href: '#relationships-card-anchor')
+      expect(page).to have_link('History', href: '#history-card-anchor')
+    end
+  end
+
 end
