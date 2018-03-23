ece30f569fe17f8ba698de0ca6a73a8b50cfb1a2
Bruno Fagundez <bruno.fagundez@osi.ca.gov>
2018-03-23 15:31:46 -0700

    Simplified smoke test to look for one element to validate deploy

--- spec/smoke/intake_dashboard_ready_spec.rb
+++ spec/smoke/intake_dashboard_ready_spec.rb
@@ -13,26 +13,8 @@ describe 'Dashboard Ready', type: :feature do
   end
 
   it 'has a Start Screening link' do
+    visit '/intake'
     expect(page).to have_button('Start Screening')
   end
 
-  it 'has a screening name column' do
-    expect(page).to have_css('th', text: 'Screening Name')
-  end
-
-  it 'has a decision column' do
-    expect(page).to have_css('th', text: 'Type/Decision')
-  end
-
-  it 'has a status column' do
-    expect(page).to have_css('th', text: 'Status')
-  end
-
-  it 'has an assignee column' do
-    expect(page).to have_css('th', text: 'Assignee')
-  end
-
-  it 'has a report date and time column' do
-    expect(page).to have_css('th', text: 'Report Date and Time')
-  end
 end
