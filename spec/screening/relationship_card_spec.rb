9ae9b01959ec684853265d838d91ae4b0a627ed7
Money <manmohit.singh@gmail.com>
2018-04-04 02:08:08 -0700

    Refactor to easily allow changing people and relationsihps
    This update will test a users ability to 'Attach' and 'Remove' relationship(s) of the screening participant to the screening.
    
    Completes: #1511

--- spec/screening/relationship_card_spec.rb
+++ spec/screening/relationship_card_spec.rb
@@ -1,89 +1,110 @@
 # frozen_string_literal: true
 
-person1 = {
-  fname: 'Marty',
-  lname: 'R'
-}
-person2 = {
-  fname: 'Ricky',
-  lname: 'W'
-}
-
 describe 'Relationship card QA test', type: :feature do
-  # Selecting Start Screening from homepage
-  # Note - This script is dependent on established seed data in the environment
-  person1_full_name, person2_full_name = nil
+  rel_1 = { fn: 'Billy', ln: 'Treppas', rel: 'Sister', rel_type: '(Half)' }
+  rel_2   = { fn: 'Joe', ln: 'Atkyns', rel: 'Daughter', rel_type: '(Alleged)' }
+  rel_3 = { fn: 'Larry', ln: 'Felgat', rel: 'Brother', rel_type: '' }
+  rel_4  = { fn: 'Phil', ln: 'Bartoleyn', rel: 'Sister', rel_type: '(Half)' }
+  p_2 = {
+    fn: 'Grandpa',
+    ln: 'Preece',
+    rel: 'Granddaughter',
+    rel_type: '(Paternal)',
+    relationships: [
+      rel_1.merge(rel: 'Grandparen', rel_type: '(Paternal)'),
+      rel_2.merge(rel: 'Father', rel_type: '(Birth)'),
+      rel_3.merge(rel: 'Grandparen', rel_type: '(Paternal)'),
+      rel_4.merge(rel: 'No Relation', rel_type: ''),
+    ]
+  }
+  p_1 = {
+    fn: 'Lucy',
+    mn: 'Lee',
+    ln: 'Gaspar',
+    relationships: [rel_1, rel_2, p_2, rel_3, rel_4]
+  }
+  p_2[:relationships].push(p_1.merge(rel: 'Grandparent', rel_type: '(Paternal)') )
 
-  before do
+  before(:each) do
     ScreeningPage.new.visit
+    autocompleter_fill_in('Search for any person', "#{p_1[:fn]} #{p_1[:ln]}")
+    wait_for_result_to_appear(element: 'div.autocomplete-menu div.profile-picture') do
+      page.find('div.autocomplete-menu').first('strong.highlighted', text: 'Lucy Lee Gaspar').double_click
+    end
   end
 
-  it 'Test initial rendering of card' do
-    within '#relationships-card' do
-      # Verify initial rendering of card
-      expect(page).to have_content('Relationships')
-      expect(page).to have_content('Add people to see their relationships here.')
-    end
-    # Adding an existing child to a screening
-    within '#search-card', text: 'Search' do
-      search_string = "#{person1[:fname]} #{person1[:lname]}"
-      autocompleter_fill_in 'Search for any person', search_string
-      matching_result = first('li', text: search_string)
-      person1_full_name = matching_result.first('strong').text
-      matching_result.click
+  describe 'Test initial rendering of card' do
+    it 'should verify initial rendering of card' do
+      within '#relationships-card' do
+        expect(page).to have_content('Relationships')
+        expect(page).to have_content('Search for people and add them to see their relationships.')
+      end
     end
 
-    within '#relationships-card' do
-      # Verify rendering of card
-      expect(page).to have_content("#{person1_full_name} is the...")
-      expect(page).to have_content('Son  of Missy R')
-      expect(page).to have_content('Son of Ricky W')
-      expect(page).to have_content('Brother of Roland W')
-      expect(page).to have_content('Brother  of Sharon W')
+    it 'should add an existing child to a screening' do
+      name = [p_1[:fn], p_1[:mn], p_1[:ln]].reject(&:blank?).join(' ')
+      within '#relationships-card' do
+        expect(page).to have_content("#{name} is the...")
+        p_1[:relationships].each do |relation|
+          expect(page).to have_content("#{relation[:rel]} #{relation[:rel_type]}   of #{relation[:fn]} #{relation[:ln]}")
+        end
+      end
+    end
+  end
+
+  describe 'existing adult' do
+    before(:each) do
+      find('li', text: /#{p_2[:fn]} #{p_2[:ln]}/).first('a').click
     end
 
-    # Adding an existing adult that is related to an existing child on a screening
-    within '#search-card', text: 'Search' do
-      search_string = "#{person2[:fname]} #{person2[:lname]}"
-      autocompleter_fill_in 'Search for any person', search_string
-      matching_result = first('li', text: search_string)
-      person2_full_name = matching_result.first('strong').text
-      matching_result.click
+    describe 'Adding participant to an existing child on a screening' do
+      it 'should add participant to the sidebar' do
+        within 'div.side-bar' do
+          expect(page).to have_css('a.link', text: "#{p_2[:fn]} #{p_2[:ln]}")
+        end
+      end
+
+      it "should remove 'Attach' link for the existing participant's relationship that is added to the screening" do
+        expect(page).not_to have_content("#{p_2[:rel]} #{p_2[:rel_type]}   of #{p_2[:fn]} #{p_2[:ln]} Attach")
+      end
+
+      it 'should verify rendering of related relations card' do
+        name = [p_2[:fn], p_2[:mn], p_2[:ln]].reject(&:blank?).join(' ')
+        within '#relationships-card' do
+          expect(page).to have_content("#{name} is the...")
+          p_2[:relationships].each do |relation|
+            expect(page).to have_content("#{relation[:rel]} #{relation[:rel_type]}   of #{relation[:fn]} #{relation[:ln]}")
+          end
+        end
+      end
+
+      it "should remove 'Attach' link for the new participant's relationship to the existing participant on the screening" do
+        expect(page).not_to have_content("#{p_2[:rel]} #{p_2[:rel_type]}   of #{p_1[:fn]} #{p_1[:ln]}")
+      end
+
     end
 
-    person2_id = find('div[id^="participants-card-"]', text: person2[:fname])[:id]
-    person2_card = find('#' + person2_id)
+    describe 'Deleting a person that was added to a screening.' do
+      before(:each) do
+        find('div.card.double-gap-bottom span', text: "#{p_2[:fn]} #{p_2[:ln]}")
+          .sibling('button', text: 'Remove').click
+      end
 
-    within '#relationships-card' do
-      # Verify rendering of card
-      expect(page).to have_content("#{person1_full_name} is the...")
-      expect(page).to have_content('Son  of Missy R')
-      expect(page).to have_content('Son of Ricky W')
-      expect(page).to have_content('Brother of Roland W')
-      expect(page).to have_content('Brother  of Sharon W')
-      expect(page).to have_content("#{person2_full_name} is the...")
-      expect(page).to have_content('Father  of Marty R')
-      expect(page).to have_content('Spouse of Missy R')
-      expect(page).to have_content('Father of Roland W')
-      expect(page).to have_content('Father  of Sharon W')
-    end
+      it 'should remove participant to the sidebar' do
+        within 'div.side-bar' do
+          expect(page).not_to have_css('a.link', text: "#{p_2[:fn]} #{p_2[:ln]}")
+        end
+      end
 
-    # Deleting a person that was added to a screening.
-    within person2_card do
-      first(:css, 'i.fa.fa-times').click
-    end
-    within '#relationships-card' do
-      # Verify rendering of card
-      expect(page).to have_content("#{person1_full_name} is the...")
-      expect(page).to have_content('Son  of Missy R')
-      expect(page).to have_content('Son of Ricky W')
-      expect(page).to have_content('Brother of Roland W')
-      expect(page).to have_content('Brother  of Sharon W')
-      expect(page).not_to have_content("#{person2_full_name} is the...")
-      expect(page).not_to have_content('Father  of Marty R')
-      expect(page).not_to have_content('Spouse of Missy R')
-      expect(page).not_to have_content('Father of Roland W')
-      expect(page).not_to have_content('Father  of Sharon W')
+      it 'should verify removal of card' do
+        name = [p_2[:fn], p_2[:mn], p_2[:ln]].reject(&:blank?).join(' ')
+        within '#relationships-card' do
+          expect(page).not_to have_content("#{name} is the...")
+          p_2[:relationships].each do |relation|
+            expect(page).not_to have_content("#{relation[:rel]} #{relation[:rel_type]}   of #{relation[:fn]} #{relation[:ln]}")
+          end
+        end
+      end
     end
   end
 end
