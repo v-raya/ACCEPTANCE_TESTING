bf4817dd47c3fb7ac08b57f2dff9e5102d762fc0
Money <manmohit.singh@gmail.com>
2018-04-30 16:23:55 -0700

    County user with sealed privileges cannot attach people
    User cannot attach people from different_county_sensitive_not_sealed and no_county_sensitive_not_sealed.

--- spec/screening/county_sealed_user_spec.rb
+++ spec/screening/county_sealed_user_spec.rb
@@ -40,4 +40,34 @@ describe 'County Sealed user', type: :feature do
         end
       end
     end
+
+    describe 'return results from search' do
+      it 'should not be able to attach people to Screening' do
+        [different_county_sensitive_not_sealed, no_county_sensitive_not_sealed].each do |participant|
+          search_name = full_name(first: participant[:first_name], last: participant[:last_name])
+          select_name = full_name(first: participant[:first_name], middle: participant[:middle_name],
+                                  last: participant[:last_name])
+
+          puts search_name
+
+          autocompleter_fill_in 'Search for any person', search_name
+          wait_for_result_to_appear(element: 'div.autocomplete-menu div.profile-picture') do
+            node = page.find('div.autocomplete-menu').first('strong.highlighted', text: select_name)
+            interact_with_node(capybara_node: node, event: 'double_click')
+          end
+
+          alert_text = page.driver.accept_modal(true)
+
+          expect(alert_text).to eq 'You are not authorized to add this person.'
+          within 'div.side-bar' do
+            expect(page).not_to have_css('a.link', text: search_name)
+          end
+
+          within 'div#relationships-card' do
+            expect(page).not_to have_css('span.person', text: select_name)
+          end
+        end
+      end
+    end
+
 end
