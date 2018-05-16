55fcad673c96b2459656830e69b8ef4f5bd775d6
Money <manmohit.singh@gmail.com>
2018-05-16 00:38:36 -0700

    Remove unused files and autoload helpers in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,17 +2,17 @@
 
 require 'byebug'
 require 'capybara/rspec'
-require 'ffaker'
-require 'helper_methods'
-require 'shame_helpers'
-require 'react_select_helpers'
-require 'datetime_helpers'
-require 'participant_helpers'
-require 'address_helpers'
-require 'screening_helpers'
+require 'faker'
 require 'selenium/webdriver'
+require 'active_support/core_ext/object/blank'
 
+%w[person victim reporter perpetrator screening_information
+   narrative incident_information allegation worker_safety
+   cross_report decision snapshot screening].each do |f|
+  require [Dir.pwd, 'lib', f].join('/')
+end
 Dir[File.dirname(__FILE__) + '/support/**/*.rb'].each { |f| require f }
+Dir[File.dirname(__FILE__) + '/helpers/**/*.rb'].each { |f| require f }
 
 Capybara.configure do |config|
   config.app_host                = ENV.fetch('APP_URL') { raise 'You must pass the app url with APP_URL=<url>.' }
@@ -32,3 +32,30 @@ Capybara.register_driver :headless_chrome do |app|
 end
 
 Capybara.javascript_driver = :headless_chrome
+
+RSpec.configure do |config|
+  include Capybara::DSL
+
+  config.before(:all, type: :feature) do
+    logout_user
+  end
+
+  config.before(:each) do
+    if Capybara.current_driver == :headless_chrome && self.class.metadata[:reset_user]
+      logout_user
+      send(self.class.metadata[:reset_user], path: self.class.metadata[:path])
+    end
+  end
+
+  config.after(:each) do
+    page.instance_variable_set(:@touched, false)
+    if Capybara.current_driver == :headless_chrome &&
+       self.class.metadata[:reset_user]
+      logout_user
+    end
+  end
+
+  config.after(:all) do
+    page.instance_variable_set(:@touched, true)
+  end
+end
