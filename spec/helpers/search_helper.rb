4e25f4b27b20ce3646079d6630dfc73eb77a2a5f
money <manmohit.singh@gmail.com>
2018-06-28 13:48:13 -0700

    Split string for client search

--- spec/helpers/search_helper.rb
+++ spec/helpers/search_helper.rb
@@ -3,7 +3,16 @@
 require_relative '../../helpers/form_helper'
 
 def search_client(label: 'Search for any person', query:)
-  Capybara.fill_in(label, with: query, fill_options: { clear: :backspace })
+  search_field = Capybara.find(:fillable_field, label)
+  if Capybara.current_driver == :selenium_edge
+    find('#search-card').click
+    backspaces = [:backspace] * search_field.value.to_s.length
+    search_field.native.send_keys(*(backspaces))
+
+    query.to_s.split(//).each { |l| search_field.set(l, clear: :none) }
+  else
+    search_field.set(query, clear: :backspace)
+  end
 end
 
 def wait_for_result_to_appear(element: 'div.autocomplete-menu')
