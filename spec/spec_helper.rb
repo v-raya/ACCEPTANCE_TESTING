adba7374568f8c77cc1772e70c3ecce5feb1a251
Money <manmohit.singh@gmail.com>
2018-07-02 10:07:37 -0700

    Skip maximizing browser window for headless chrome

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -48,7 +48,9 @@ end
 
 Capybara.javascript_driver = :headless_chrome
 
-Capybara.page.driver.browser.manage.window.maximize
+unless Capybara.current_driver == :headless_chrome
+  Capybara.page.driver.browser.manage.window.maximize
+end
 
 # capybara session
 module Capybara
